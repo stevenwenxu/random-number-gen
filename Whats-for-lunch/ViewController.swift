fcc3b0fc14d29061fe077ed561bab57f592280a7
Steven-xu94 <steven.xu13@gmail.com>
2016-02-09 21:45:09 -0500

    Initial Commit

---
+++ Whats-for-lunch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats-for-lunch
+//
+//  Created by Steven Xu on 2016-02-09.
+//  Copyright © 2016 Steven Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+

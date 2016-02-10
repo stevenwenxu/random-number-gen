9e5b3ecd2a678e421c2aca374288745e8f0a7542
Steven-xu94 <steven.xu13@gmail.com>
2016-02-09 23:41:43 -0500

    Initial commit

--- Whats-for-lunch/ViewController.swift
+++ Whats-for-lunch/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+	@IBOutlet weak var fromTextField: UITextField!
+	@IBOutlet weak var toTextField: UITextField!
+	@IBOutlet weak var generateBtn: UIButton!
+	@IBOutlet weak var result: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		self.fromTextField.delegate = self
+		self.toTextField.delegate = self
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	func textFieldShouldReturn(textField: UITextField) -> Bool {
+		return true
 	}
 
+	func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+		var from = self.fromTextField.text!
+		var to = self.toTextField.text!
+		if self.fromTextField.isFirstResponder() {
+			from = string == "" ? (from as NSString).stringByReplacingCharactersInRange(range, withString: string) : "\(from)" + string
+		} else if self.toTextField.isFirstResponder() {
+			to = string == "" ? (to as NSString).stringByReplacingCharactersInRange(range, withString: string) : "\(to)" + string
+		}
+		self.generateBtn.enabled = Int(to) >= Int(from)
+		return true
+	}
 
+	@IBAction func generatePressed(sender: UIButton) {
+		if let from = UInt32(self.fromTextField.text!), let to = UInt32(self.toTextField.text!) where to >= from {
+			self.generateBtn.enabled = true
+			let rand = Int(arc4random_uniform(to - from + 1)) + Int(from)
+			self.result.text = "\(rand)"
+		} else {
+			self.generateBtn.enabled = false
+		}
+		self.fromTextField.resignFirstResponder()
+		self.toTextField.resignFirstResponder()
+	}
 }
 
